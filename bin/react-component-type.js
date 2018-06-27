6e40c214835415e3c7402c642f7df2d8255edafb
韬韫 <zhantao.zt@koubei.com>
2018-06-27 17:29:26 +0800

    fix: update bin/react-component-type.js

--- bin/react-component-type.js
+++ bin/react-component-type.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const parse = require('../src/parse')
 
 program
   .command('parse <dir>', 'A component or directory')
   .option('-o, --output', 'output path')
   .action(function (dir, cmd) {
-    console.log('parse ' + dir + (cmd.output ? ' output' : ''))
+    parse(dir)
   })
 
 program.parse(process.argv)
